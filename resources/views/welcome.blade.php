2689a48c0148fdd8a5886094385be83a73c31623
yuantamang <yuantamang@gmail.com>
2024-07-14 19:35:29 +0545

    change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,15 +31,27 @@
             <img class="h-auto max-w-full rounded-lg" src="/image/photo 2.jpg" alt="image"loading="_lazy">
         </div>
 
-        <div class="m-3 p-3 lg:text-base lg:font-medium">
+        <!-- button -->
+        
+        <div class="m-3 p-3 lg:text-base lg:font-medium grid gap-4 lg:flex xl:flex">
+            <div>
         <a href="/Wedding" class="py-2 px-3 text-xs font-thin text-right   text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">Wedding Photos</a>
-        <a href="/Landscape" class="py-2 px-3 text-xs font-thin text-right   text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">Landscape Photos</a>
-        <a href="/PostPhoto" class="py-2 px-3 text-xs font-thin text-right   text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">Post Photos</a>
-        <a href="/Document" class="py-2 px-3 m-4 text-xs font-thin text-right   text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">Documets</a>
-        <a href="/event" class="py-2 px-3 m-4 text-xs font-thin text-right   text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">Event Photos</a>
-
-
+            </div>
+            <div>
+                <a href="/Landscape" class="py-2 px-3 text-xs font-thin text-right   text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">Landscape Photos</a>
+            </div>
+            <div>
+                <a href="/PostPhoto" class="py-2 px-3 text-xs font-thin text-right   text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">Post Photos</a>
+            </div>
+            <div>
+                <a href="/Document" class="py-2 px-3 m-4 text-xs font-thin text-right   text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">Documets</a>
+            </div>
+            <div>
+                <a href="/event" class="py-2 px-3 m-4 text-xs font-thin text-right   text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">Event Photos</a>
+            </div>
     </div>
+
+    <!-- button end -->
 
     <div class="grid grid-cols-5 gap-4">
          <div>
